5b3c5e3cc2cffba05f8b02ad96c1e55e2252c222
Filip Janješić <filip.janjesic@gmail.com>
2023-08-10 10:15:35 +0200

    update

--- Basic defense game/index.js
+++ Basic defense game/index.js
@@ -1,1 +1,17 @@
-const canvas = document.querySelector('element');
+const canvas = document.
+    querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+}
+
+const player = new Player(100, 100, 30, 'blue')
